e6b05560bfdce0ea952548960f252ec5bcfc1862
Chase Manning <chase@manning.dev>
2025-05-24 17:12:39 +0100

    :art: change get decks to use new functions

--- analysis/src/utils/get-decks.ts
+++ analysis/src/utils/get-decks.ts
@@ -1,14 +1,10 @@
 import fs from "fs";
-import getDeckName from "./get-deck-name";
 import getMultiplier from "./get-multiplier";
-import {
-  NOEX,
-  EXPANSION_RELEASE_DATE,
-  POST_EXPANSION_PERCENT,
-  NOEX_PERCENT_CUTOFF,
-  WIGGLYTUFF_PERCENT_CUTOFF,
-  NO_TRAINER_PERCENT_CUTOFF,
-} from "../settings";
+import { filterDecks } from "./filter-decks";
+import { populateDeckNames } from "./populate-deck-names";
+import { updateDeckResults } from "./update-deck-results";
+import { calculateMultipliers } from "./calculate-multipliers";
+import { fixPokeballCards } from "./fix-pokeball-cards";
 import { Deck } from "./types";
 
 const getDecks = () => {
@@ -16,67 +12,27 @@ const getDecks = () => {
     fs.readFileSync("./data/decks.json", "utf-8")
   );
 
-  const decksWithoutNames = decksWithoutNames_
-    .filter(
-      (deck) => !deck.cards.some((card) => card.name.endsWith(" ex")) || !NOEX
-    )
-    .filter((deck) => {
-      const isNoEx = deck.tournamentExPercent < NOEX_PERCENT_CUTOFF;
-      return isNoEx === NOEX;
-    })
-    .filter((deck) => deck.wigglytuffPercent < WIGGLYTUFF_PERCENT_CUTOFF)
-    .filter((deck) => deck.noTrainerPercent < NO_TRAINER_PERCENT_CUTOFF);
+  const filteredDecks = filterDecks(decksWithoutNames_);
+  const { decks: decksWithNames, idToName } = populateDeckNames(filteredDecks);
+  const decksWithResults = updateDeckResults(decksWithNames, idToName);
 
-  // Populating deck names
-  let decksWithNames = [];
-  const idToName: Record<string, string> = {};
-  for (const deck of decksWithoutNames) {
-    const name = getDeckName(deck);
-    if (!name) continue;
-    idToName[deck.id] = name;
-    decksWithNames.push({
-      ...deck,
-      name,
-    });
-  }
-
-  // Updating wins and losses with deck names
-  decksWithNames = decksWithNames.map((deck) => {
-    return {
-      ...deck,
-      wins: deck.wins.map((win) => idToName[win]).filter((name) => name),
-      losses: deck.losses.map((loss) => idToName[loss]).filter((name) => name),
-    };
-  });
-
-  const totalGames = decksWithNames.reduce(
+  const totalGames = decksWithResults.reduce(
     (acc, deck) => acc + deck.totalGames,
     0
   );
   console.log("Sample Games:", (totalGames / 2).toLocaleString());
 
-  let dates = decksWithNames.map((deck) => new Date(deck.date));
+  let dates = decksWithResults.map((deck) => new Date(deck.date));
   dates = dates.filter((date, index) => dates.indexOf(date) === index);
   const newestDate = dates.reduce(
     (acc, date) => (date > acc ? date : acc),
     new Date(0)
   );
-  const totalDeckCount = decksWithNames.length;
-  const deckCountBeforeExpansion = decksWithNames.filter(
-    (deck) => new Date(deck.date) < EXPANSION_RELEASE_DATE
-  ).length;
-  const deckCountAfterExpansion = decksWithNames.filter(
-    (deck) => new Date(deck.date) >= EXPANSION_RELEASE_DATE
-  ).length;
-  const targetDeckCountAfterExpansion = totalDeckCount * POST_EXPANSION_PERCENT;
-  const targetDeckCountBeforeExpansion =
-    totalDeckCount * (1 - POST_EXPANSION_PERCENT);
-  const beforeExpansionMul =
-    targetDeckCountBeforeExpansion / deckCountBeforeExpansion;
-  const afterExpansionMul =
-    targetDeckCountAfterExpansion / deckCountAfterExpansion;
 
-  let output = decksWithNames.map((deck) => {
+  const { beforeExpansionMul, afterExpansionMul } =
+    calculateMultipliers(decksWithResults);
+
+  let output = decksWithResults.map((deck) => {
     const multiplier = getMultiplier(
       deck,
       newestDate,
@@ -90,23 +46,7 @@ const getDecks = () => {
     };
   });
 
-  // Fixing issue with double Poke Balls
-  return output.map((deck) => {
-    const cards = deck.cards.map((card) => {
-      if (card.name === "Poké Ball" && card.number === "111") {
-        return {
-          ...card,
-          set: "P-A",
-          number: "5",
-        };
-      }
-      return card;
-    });
-    return {
-      ...deck,
-      cards,
-    };
-  });
+  return fixPokeballCards(output);
 };
 
 export default getDecks;
