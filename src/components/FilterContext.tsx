202b422cf13cbd1bc38e18cb968e5097ffc1f071
Chase Manning <chase@manning.dev>
2025-05-30 08:28:28 +0100

    :sparkles: add show ex to filters

--- src/components/FilterContext.tsx
+++ src/components/FilterContext.tsx
@@ -3,11 +3,15 @@ import { createContext, useState } from "react";
 interface FilterContextType {
   energy: string | null;
   setEnergy: (energy: string | null) => void;
+  includeEx: boolean;
+  setIncludeEx: (include: boolean) => void;
 }
 
 export const FilterContext = createContext<FilterContextType>({
   energy: null,
   setEnergy: () => {},
+  includeEx: true,
+  setIncludeEx: () => {},
 });
 
 interface Props {
@@ -16,14 +20,18 @@ interface Props {
 
 const FilterContextProvider = ({ children }: Props) => {
   const [energy, setEnergy] = useState<string | null>(null);
+  const [includeEx, setIncludeEx] = useState<boolean>(true);
 
   return (
     <FilterContext.Provider
       value={{
         energy,
         setEnergy: (energy) => {
-          console.log("setting energy", energy);
           setEnergy(energy);
+        },
+        includeEx,
+        setIncludeEx: (include) => {
+          setIncludeEx(include);
         },
       }}
     >
