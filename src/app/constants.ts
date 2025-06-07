b303f14124ea15c1e06d6fa6a6e65521fed09877
Chase Manning <chase@manning.dev>
2025-06-07 13:30:13 +0100

    :sparkles: add constants

---
+++ src/app/constants.ts
@@ -0,0 +1,21 @@
+import github from "../assets/github.svg";
+import twitter from "../assets/twitter.svg";
+
+export const GITHUB_URL =
+  "https://github.com/chase-manning/pokemon-tcg-pocket-tier-list";
+export const TWITTER_URL = "https://x.com/pocketdecks";
+
+export const SOCIALS = [
+  {
+    url: GITHUB_URL,
+    icon: github,
+    alt: "GitHub",
+    label: "View source code on GitHub",
+  },
+  {
+    url: TWITTER_URL,
+    icon: twitter,
+    alt: "Twitter",
+    label: "Follow us on Twitter",
+  },
+];
