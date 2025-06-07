8af31fa19cea2bb760a3ab5a265d05ca0029b159
Chase Manning <chase@manning.dev>
2025-06-07 15:01:56 +0100

    :rocket: add features section

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Hero from "./Hero";
 import Header from "../../components/Header";
+import Features from "./Features";
 
 const StyledLandingPage = styled.div`
   width: 100%;
@@ -17,6 +18,7 @@ const LandingPage = () => {
   return (
     <StyledLandingPage>
       <Hero />
+      <Features />
       <Header />
     </StyledLandingPage>
   );
