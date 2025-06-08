8f9286dfcea7c123f725fd35cc24888b8380de22
Chase Manning <chase@manning.dev>
2025-06-08 10:16:16 +0100

    :sparkles: add stripe constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,6 +4,10 @@ import twitter from "../assets/twitter.svg";
 export const GITHUB_URL =
   "https://github.com/chase-manning/pokemon-tcg-pocket-tier-list";
 export const TWITTER_URL = "https://x.com/pocketdecks";
+export const MONTHLY_PRICE_ID = "price_1RXU5FBIHa8JB5eJAkWt6cRp";
+export const YEARLY_PRICE_ID = "price_1RXUYrBIHa8JB5eJ9XMAnFia";
+export const MANAGE_SUBSCRIPTION_URL =
+  "https://billing.stripe.com/p/login/4gM9ASeDk2QcaKq2Y957W00";
 
 export const SOCIALS = [
   {
