75a71d39aeb4562145407219f49e004cdc3fa9ec
Chase Manning <chase@manning.dev>
2025-06-10 10:18:28 +0100

    :sparkles: add opponents list

---
+++ analysis/src/opponents.ts
@@ -0,0 +1,21 @@
+export type Oponent = string | string[];
+
+const OPPONENTS: Oponent[] = [
+  ["Rampardos A2 89", "Silvally A3a 61"],
+  ["Charizard ex A2b 10", "Silvally A3a 61"],
+  ["Solgaleo ex A3 122", "Shiinotic A3a 27"],
+  ["Guzzlord ex A3a 43", "Naganadel A3a 45"],
+  ["Silvally A3a 61", "Blacephalon A3a 9"],
+  ["Incineroar ex A3 33", "Charizard ex A2b 10"],
+  ["Silvally A3a 61", "Oricorio A3 66"],
+  ["Buzzwole ex A3a 6", "Celesteela A3a 62"],
+  ["Rampardos A2 89", "Silvally A3a 61"],
+  ["Buzzwole ex A3a 6", "Celesteela A3a 62"],
+  ["Tapu Koko ex A3a 19", "Zeraora A3a 21"],
+  ["Rampardos A2 89", "Silvally A3a 61"],
+  ["Solgaleo ex A3 122", "Shiinotic A3a 27"],
+  ["Tapu Koko ex A3a 19", "Oricorio A3 66"],
+  ["Giratina ex A2b 35", "Darkrai ex A2 110"],
+];
+
+export default OPPONENTS;
