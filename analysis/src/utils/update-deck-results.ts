e89b443f55f3fae7364510cdd97a2400cfc74f27
Chase Manning <chase@manning.dev>
2025-06-10 10:19:59 +0100

    :rocket: add oppponent battle option

--- analysis/src/utils/update-deck-results.ts
+++ analysis/src/utils/update-deck-results.ts
@@ -1,12 +1,46 @@
 import { Deck } from "./types";
+import OPPONENTS, { Oponent } from "../opponents";
+import { OPPONENT_BATTLE } from "../settings";
+import { formatMatch } from "./format-name";
+
+const convertToNames = (ids: string[], idToName: Record<string, string>) => {
+  return ids.map((id) => idToName[id]).filter((name) => name);
+};
 
 export const updateDeckResults = (
   decks: Deck[],
   idToName: Record<string, string>
 ): Deck[] => {
+  // If it's an opponent battle, we update the winCount and lossCount to only match the local opponents
+  if (OPPONENT_BATTLE) {
+    const opponents = OPPONENTS.map((opponent: Oponent) => {
+      const match = Array.isArray(opponent) ? opponent : [opponent];
+      return formatMatch(match);
+    });
+    return decks.map((deck) => {
+      const wins = convertToNames(deck.wins, idToName);
+      const losses = convertToNames(deck.losses, idToName);
+      let winCount = 0;
+      let lossCount = 0;
+      for (const win of wins) {
+        winCount += opponents.filter((opponent) => opponent === win).length;
+      }
+      for (const loss of losses) {
+        lossCount += opponents.filter((opponent) => opponent === loss).length;
+      }
+      return {
+        ...deck,
+        winCount,
+        lossCount,
+        wins,
+        losses,
+      };
+    });
+  }
+
   return decks.map((deck) => ({
     ...deck,
-    wins: deck.wins.map((win) => idToName[win]).filter((name) => name),
-    losses: deck.losses.map((loss) => idToName[loss]).filter((name) => name),
+    wins: convertToNames(deck.wins, idToName),
+    losses: convertToNames(deck.losses, idToName),
   }));
 };
