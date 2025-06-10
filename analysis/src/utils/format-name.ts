d43826efe3623ff102adf6a20b10d8b0ae29615f
Chase Manning <chase@manning.dev>
2025-06-10 10:18:47 +0100

    :sparkles: add format match function

--- analysis/src/utils/format-name.ts
+++ analysis/src/utils/format-name.ts
@@ -18,3 +18,19 @@ const formatName = (cards: Card[], match: string[]): string => {
 };
 
 export default formatName;
+
+export const formatMatch = (match: string[]) => {
+  return match
+    .map((cardName) => {
+      const cardNameParts = cardName.split(" ");
+      const id = cardNameParts[cardNameParts.length - 1];
+      const setRaw = cardNameParts[cardNameParts.length - 2];
+      const set = setRaw === "P-A" ? "PA" : setRaw;
+      const idLength = id.length;
+      const nameLength = cardName.length - idLength - setRaw.length - 2;
+      const name = cardName.slice(0, nameLength);
+      const paddedId = id.padStart(3, "0");
+      return `${name}-${set}-${paddedId}`;
+    })
+    .join("&");
+};
