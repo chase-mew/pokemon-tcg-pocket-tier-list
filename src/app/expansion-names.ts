0957c98400c63e099d303073c5246d53030ca3d0
Chase Manning <chase@manning.dev>
2025-06-30 10:37:31 +0100

    :sparkles: add expansion name data

---
+++ src/app/expansion-names.ts
@@ -0,0 +1,17 @@
+export const EXPANSION_NAMES = {
+  a1: "Genetic Apex",
+  a1a: "Mythical Island",
+  pa: "Promo-A",
+  a2: "Space-Time Smackdown",
+  a2a: "Triumphant Light",
+  a2b: "Shining Revelry",
+  a3: "Celestial Guardians",
+  a3a: "Extradimensional Crisis",
+  a3b: "Eevee Grove",
+};
+
+export const EXPANSION_CODES = Object.keys(EXPANSION_NAMES);
+
+export const getExpansionName = (expansion: string) => {
+  return EXPANSION_NAMES[expansion as keyof typeof EXPANSION_NAMES];
+};
