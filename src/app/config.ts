8023ab8d85afadd133aced9229a92f16fe2e69b4
Chase Manning <chase@manning.dev>
2025-10-05 08:56:36 +0100

    :wrench: update min matchup games to 30

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const DEBUG = false;
 export const MIN_PERCENT_TO_QUALIFY = 0.004;
-export const MIN_MATCHUP_GAMES = 100;
+export const MIN_MATCHUP_GAMES = 30;
 export const WINRATE_THRESHOLD = 0.505;
